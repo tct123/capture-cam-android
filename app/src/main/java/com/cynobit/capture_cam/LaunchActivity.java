ab73eb5393d1a9e2ec886ad1b77cd95faf78d14d
Francis Ilechukwu <francis94c@gmail.com>
2018-11-27 21:04:27 +0100

    Took care of the Launcher.

--- app/src/main/java/com/cynobit/capture_cam/LaunchActivity.java
+++ app/src/main/java/com/cynobit/capture_cam/LaunchActivity.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cynobit.capture_cam.OnActivityResultContract.*;
+
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainActivity.class));
+        startActivityForResult(new Intent(this, MainActivity.class), RequestCodes.MAIN_ACTIVITY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RequestCodes.MAIN_ACTIVITY) {
+            finish();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
